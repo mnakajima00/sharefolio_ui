cd2e182fa6e9e33a545b229591db707355d96cbb
sarahstuhr <92468640+sarahstuhr@users.noreply.github.com>
2022-08-13 18:50:05 -0400

    fix: full width topbar and responsive icons

--- components/common/TopBar/index.tsx
+++ components/common/TopBar/index.tsx
@@ -37,7 +37,7 @@ const TopBar = () => {
                 href='/'
                 sx={{
                     mr: 2,
-                    display: { xs: 'none', md: 'flex' },
+                    display: { xs: 'none', sm: 'flex' },
                     fontWeight: 900,
                     fontSize: '2em',
                     textDecoration: 'none',
@@ -50,7 +50,7 @@ const TopBar = () => {
     )
     return (
         <AppBar position="fixed" className=".appbar-shadow" sx={{ backgroundColor: 'background.default' }}>
-            <Container maxWidth="xl">
+            <Container maxWidth={false}>
                 <Toolbar disableGutters>
                     {/* LOGO AND TEXT - FOR > xs */}
                     {/* LOGO GOES HERE */}
@@ -61,7 +61,7 @@ const TopBar = () => {
                         href='/'
                         sx={{
                             mr: 2,
-                            display: { xs: 'none', md: 'flex' },
+                            display: { xs: 'none', sm: 'flex' },
                             fontWeight: 900,
                             fontSize: '2em',
                             textDecoration: 'none',
@@ -81,10 +81,10 @@ const TopBar = () => {
                         href='/'
                         sx={{
                             mr: 2,
-                            display: { xs: 'flex', md: 'none' },
+                            display: { xs: 'flex', sm: 'none' },
                             flexGrow: 1,
                             fontWeight: 900,
-                            fontSize: '2em',
+                            fontSize: {xs: '1.5em', sm: '2em'},
                             textDecoration: 'none',
                             color: 'primary.main'
                         }}
@@ -92,26 +92,25 @@ const TopBar = () => {
                         ShareFolio<Box component="span" sx={{ color: 'secondary.main' }}>.</Box>
                     </Typography>
 
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', sm: 'flex' } }}>
                         
                     </Box>
 
                     <Box sx={{ flexGrow: 0 }}>
-                        <Box component="span" sx={{ mr: '2em' }}>
+                        <Box component="span" sx={{ mr: { xs: '2em'} }}>
                             {/* NOTIFICATIONS */}
-                            <IconButton sx={{ mr: '1em' }}>
+                            <IconButton sx={{ mr: { xs: '0.5em' },width: { xs: 32, sm: 48 }, height: { xs: 32, sm: 48 } }}>
                                 <IoNotificationsOutline />
                             </IconButton>
                             {/* INBOX/MESSAGES */}
-                            <IconButton>
+                            <IconButton sx={{ width: { xs: 32, sm: 48 }, height: { xs: 32, sm: 48 } }}>
                                 <IoMailOutline />
                             </IconButton>
                         </Box>
                         
                         <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                             {/* ADD USER AVATAR HERE */}
-                            <Avatar alt="Remy Sharp" src="" sx={{ backgroundColor: "primary.main" }} />
-
+                            <Avatar alt="Remy Sharp" src="" sx={{ backgroundColor: "primary.main", width: { xs: 32, sm: 48 }, height: { xs: 32, sm: 48 }}} />
                         </IconButton>
                         <Menu
                             sx={{ mt: '0.5em' }}
@@ -130,6 +129,7 @@ const TopBar = () => {
                             open={Boolean(anchorElUser)}
                             onClose={handleCloseUserMenu}
                         >
+                            {/* PROFILE PIC & NAME */}
                             <Stack direction="row" spacing={2} alignItems="center" sx={{ mb: '1em', paddingLeft: '2em', paddingRight: '2em' }}>
                                 <Box>
                                     <Avatar alt="Remy Sharp" src="" sx={{ backgroundColor: "primary.main" }} />
@@ -138,9 +138,13 @@ const TopBar = () => {
                                     <Typography sx={{ fontWeight: 500 }}>Maiku Nakajima</Typography>
                                 </Box>
                             </Stack>
+
                             <Box sx={{ marginTop: '0.5em', marginBottom: '0.5em' }}>
                                 <Divider />
                             </Box>
+
+                            {/* MESSAGES AND NOTIFICATIONS */}
+
                             <MenuItem onClick={handleCloseUserMenu}>
                                 <Stack direction="row" spacing={1} alignItems="center" justifyContent="center">
                                     <IoPersonOutline />
